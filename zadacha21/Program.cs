456e4254bce353573b64b91867aeeef2017709fa
Olga <oborovikova_80@mail.ru>
2023-07-24 21:07:34 +0300

    zadacha21

--- zadacha21/Program.cs
+++ zadacha21/Program.cs
@@ -3,48 +3,23 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// int x1 = ReadInt("Введите координату X первой точки: ");
-// int y1 = ReadInt("Введите координату Y первой точки: ");
-// int z1 = ReadInt("Введите координату Z первой точки: ");
-// int x2 = ReadInt("Введите координату X второй точки: ");
-// int y2 = ReadInt("Введите координату Y второй точки: ");
-// int z2 = ReadInt("Введите координату Z второй точки: ");
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
 
-// int A = x2 - x1;
-// int B = y2 - y1;
-// int C = z1 - z2;
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
 
-// double lenght = Math.Sqrt(A*A+B*B+C*C);
-// Console.WriteLine($"Длинна отрезка {length}");
+double lenght = Math.Sqrt(A*A+B*B+C*C);
+Console.WriteLine($"Длинна отрезка {length}");
 
-// intReadInt("string message")
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
-
-// Console.WriteLine("\n Задача 21");
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
-
-int Coordinate(string coorName, string pointName)
+intReadInt("string message")
 {
-    Console.Write($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt16(Console.ReadLine());
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
-double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
-}
-
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
-
-Console.WriteLine($"Длина отрезка  {segmentLength}");
